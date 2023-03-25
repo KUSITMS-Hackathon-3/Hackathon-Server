1d44ec10257dffca6dcd3084195cb692ec6be5a0
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-25 23:07:22 +0900

    feat: 레시피 재료 엔티티

--- src/main/java/com/example/hackathon/domain/ringredient/entity/Ringredient.java
+++ src/main/java/com/example/hackathon/domain/ringredient/entity/Ringredient.java
@@ -1,4 +1,29 @@
 package com.example.hackathon.domain.ringredient.entity;
 
+import com.example.hackathon.domain.recipe.entity.Recipe;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Ringredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ingredientIdx;
+
+    private String element;
+
+    @ManyToOne
+    @JoinColumn(name = "recipeIdx")
+    private Recipe recipe;
+
+    public void addRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
 }
---
+++ src/main/java/com/example/hackathon/domain/ringredient/repository/RingredientRepository.java
@@ -0,0 +1,7 @@
+package com.example.hackathon.domain.ringredient.repository;
+
+import com.example.hackathon.domain.ringredient.entity.Ringredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RingredientRepository extends JpaRepository<Ringredient, Long> {
+}
