a1d6e9e29a8290c233da8266181caadd8ccab601
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-26 00:39:37 +0900

    feat: 날짜별 인증 확인

--- src/main/java/com/example/hackathon/domain/certification/repository/CertificationRepository.java
+++ src/main/java/com/example/hackathon/domain/certification/repository/CertificationRepository.java
@@ -3,6 +3,9 @@ package com.example.hackathon.domain.certification.repository;
 import com.example.hackathon.domain.certification.entity.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CertificationRepository extends JpaRepository<Certification, Long> {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+    List<Certification> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/com/example/hackathon/domain/certification/service/CertificationService.java
+++ src/main/java/com/example/hackathon/domain/certification/service/CertificationService.java
@@ -1,5 +1,6 @@
 package com.example.hackathon.domain.certification.service;
 
+import com.example.hackathon.domain.certification.dto.CertificationDateDto;
 import com.example.hackathon.domain.certification.dto.CertificationInsertDto;
 import com.example.hackathon.domain.certification.entity.Certification;
 import com.example.hackathon.domain.certification.repository.CertificationRepository;
@@ -8,7 +9,10 @@ import com.example.hackathon.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import javax.swing.text.DateFormatter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +38,30 @@ public class CertificationService {
          * 필요 :
          * 인증시 점수 업
          */
+    }
+
+    public CertificationDateDto findDateCertification(String startDay) {
+        startDay += "-00-00";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm");
+
+        LocalDateTime start = LocalDateTime.parse(startDay, formatter);
+        LocalDateTime end = start.plusDays(1L);
+
+        List<Certification> certifications = certificationRepository.findAllByCreatedAtBetween(start, end);
+        List<Map<String, String>> maps = new ArrayList<>();
+
+        for (Certification certification : certifications) {
+            Map<String, String> map = new HashMap<>();
+            map.put("imageUrl", certification.getImageUrl());
+            map.put("memo", certification.getMemo());
+            maps.add(map);
+        }
+
+        CertificationDateDto certificationDateDto = CertificationDateDto.builder()
+                .date(startDay)
+                .contents(maps)
+                .build();
+        return certificationDateDto;
     }
 
 }
