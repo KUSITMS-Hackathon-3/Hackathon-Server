41499f7f7f11d745a8952fdfb6ec45c4956ebb67
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-25 17:25:11 +0900

    feat: Board Entity 생성

--- src/main/java/com/example/hackathon/domain/board/entity/Board.java
+++ src/main/java/com/example/hackathon/domain/board/entity/Board.java
@@ -1,4 +1,22 @@
 package com.example.hackathon.domain.board.entity;
 
+import com.example.hackathon.domain.user.entity.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardIdx;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "userIdx")
+    private User user;
 }
