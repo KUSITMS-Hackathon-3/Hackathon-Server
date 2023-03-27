4af56bdd98309807c4f10e4c03722ea01f4934ae
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-28 01:23:48 +0900

    feat: refreshToken 저장 및 재발급

--- src/main/java/com/example/hackathon/global/config/security/jwt/JwtFilter.java
+++ src/main/java/com/example/hackathon/global/config/security/jwt/JwtFilter.java
@@ -1,11 +1,18 @@
 package com.example.hackathon.global.config.security.jwt;
 
+import com.example.hackathon.domain.user.dto.UserDto;
+import com.example.hackathon.domain.user.entity.User;
+import com.example.hackathon.domain.user.repository.UserRepository;
+import com.example.hackathon.domain.user.service.UserServiceImpl;
 import com.example.hackathon.global.config.security.jwt.JwtConstants.JWTExceptionList;
+import com.example.hackathon.global.dto.TokenInfoResponse;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -21,17 +28,26 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN = "RefreshToken";
     private final JwtTokenProvider tokenProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String jwt = resolveToken(request);
         String requestURI = request.getRequestURI();
+        String refreshToken = resolveRefreshToken(request);
         try {
+            if (StringUtils.hasText(refreshToken) && tokenProvider.validateRefreshToken(refreshToken)) {
+                Long userIdx = tokenProvider.getUserIdx(refreshToken);
+                Authentication authentication = tokenProvider.getAuthentication(userIdx);
+                TokenInfoResponse token = tokenProvider.createToken(authentication);
+                response.setHeader("accessToken", token.getAccessToken());
+                response.setHeader("refreshToken", token.getAccessToken());
+            }
             if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
                 Authentication authentication = tokenProvider.getAuthentication(jwt);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+                log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
             }
         } catch (SecurityException | MalformedJwtException e) {
             request.setAttribute("exception", JWTExceptionList.WRONG_TYPE_TOKEN.getErrorCode());
@@ -46,9 +62,8 @@ public class JwtFilter extends OncePerRequestFilter {
             log.error("JwtFilter - doFilterInternal() 오류발생");
             log.error("token : {}", jwt);
             log.error("Exception Message : {}", e.getMessage());
-            log.error("Exception StackTrace : {");
+            log.error("Exception StackTrace : ");
             e.printStackTrace();
-            log.error("}");
             log.error("================================================");
             request.setAttribute("exception", JWTExceptionList.UNKNOWN_ERROR.getErrorCode());
         }
@@ -60,6 +75,14 @@ public class JwtFilter extends OncePerRequestFilter {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private String resolveRefreshToken(HttpServletRequest request) {
+        String bearerRefreshToken = request.getHeader(REFRESH_TOKEN);
+        if (StringUtils.hasText(bearerRefreshToken) && bearerRefreshToken.startsWith("Bearer ")) {
+            return bearerRefreshToken.substring(7);
         }
         return null;
     }
--- src/main/java/com/example/hackathon/global/config/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/hackathon/global/config/security/jwt/JwtTokenProvider.java
@@ -1,5 +1,8 @@
 package com.example.hackathon.global.config.security.jwt;
 
+import com.example.hackathon.domain.user.dto.UserDto;
+import com.example.hackathon.domain.user.service.UserServiceImpl;
+import com.example.hackathon.global.config.security.redis.RedisRepository;
 import com.example.hackathon.global.dto.TokenInfoResponse;
 import com.example.hackathon.domain.user.entity.User;
 import com.example.hackathon.domain.user.exception.NotFoundUserIdException;
@@ -11,23 +14,31 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider implements InitializingBean {
+    private final RedisRepository redisRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
     private static final String AUTHORITIES_KEY = "auth";
+    private static final String USER_IDX = "userIdx";
 
     @Value("${jwt.secret}")
     private String secret;
@@ -58,6 +69,9 @@ public class JwtTokenProvider implements InitializingBean {
         Date accessTokenValidity = new Date(now + this.accessTokenValidityTime);
         Date refreshTokenValidity = new Date(now + this.refreshTokenValidityTime);
 
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+        Long userIdx = principal.getUser().getUserIdx();
+
         String accessToken = Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim(AUTHORITIES_KEY, authorities)
@@ -67,11 +81,26 @@ public class JwtTokenProvider implements InitializingBean {
 
         String refreshToken = Jwts.builder()
                 .setExpiration(refreshTokenValidity)
+                .claim(USER_IDX, userIdx)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
-        return TokenInfoResponse.from("Bearer", accessToken, refreshToken, refreshTokenValidityTime);
+        /**
+         * refreshToken redis에 저장
+         */
+        updateRefreshToken(userIdx, refreshToken);
 
+        return TokenInfoResponse.from("Bearer", accessToken, refreshToken, refreshTokenValidityTime);
+    }
+
+    public void updateRefreshToken(Long userIdx, String refreshToken) {
+        try {
+            log.info("refreshToken 저장");
+            redisRepository.setValues(String.valueOf(userIdx), refreshToken, Duration.ofSeconds(refreshTokenValidityTime));
+        } catch (NoSuchElementException e) {
+            log.error("일치하는 회원이 없습니다.");
+            throw e;
+        }
     }
 
     public Authentication getAuthentication(String accessToken) {
@@ -113,7 +142,11 @@ public class JwtTokenProvider implements InitializingBean {
 
     public boolean validateRefreshToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            Object userIdx = Jwts.parserBuilder().setSigningKey(key).build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .get(USER_IDX);
+            redisRepository.getValues(userIdx.toString()).orElseThrow();
             return true;
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
             log.info("잘못된 JWT 서명입니다.");
@@ -127,8 +160,9 @@ public class JwtTokenProvider implements InitializingBean {
         } catch (IllegalArgumentException e) {
             log.info("JWT 토큰이 잘못되었습니다.");
             throw e;
-        } finally {
-            return false;
+        } catch (NoSuchElementException e) {
+            log.error("유효하지 않은 JWT 입니다.");
+            throw e;
         }
     }
 
@@ -140,10 +174,31 @@ public class JwtTokenProvider implements InitializingBean {
         }
     }
 
+    public Long getUserIdx(String refreshToken) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(refreshToken).getBody();
+        Long userIdx = claims.get(USER_IDX, Long.class);
+        return userIdx;
+    }
+
     public Long getExpiration(String accessToken) {
         Date expiration = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody().getExpiration();
         Long now = new Date().getTime();
         return (expiration.getTime() - now);
     }
+
+    public Authentication getAuthentication(Long userIdx) {
+        try {
+            User user = userRepository.findById(userIdx).orElseThrow();
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getUserId(), user.getPassword());
+            Authentication authentication = this.authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+            return authentication;
+        } catch (NoSuchElementException e) {
+            log.error("올바르지 않은 userIdx 입니다.");
+            throw e;
+        }
+    }
 }
+
 
