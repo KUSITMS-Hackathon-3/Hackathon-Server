b321691f8ff9868175c7e88cea113176107ae342
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-04-04 16:28:30 +0900

    feat: 재발급 요청 처리

--- src/main/java/com/example/hackathon/domain/user/controller/UserController.java
+++ src/main/java/com/example/hackathon/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.hackathon.domain.user.dto.UserDto.LoginRequest;
 import com.example.hackathon.domain.user.dto.UserDto.LoginResponse;
 import com.example.hackathon.domain.user.dto.UserDto.SignupRequest;
 import com.example.hackathon.domain.user.service.UserService;
+import com.example.hackathon.global.dto.TokenInfoResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -43,4 +44,10 @@ public class UserController {
     /**
      * 아이디 비밀번호 일치하지 않는경우 체크하도록 만들었어
      */
+
+    @PostMapping("/reIssueAccessToken/{userIdx}")
+    public ResponseEntity<ResponseDto<LoginResponse>> reIssueAccessToken(@PathVariable Long userIdx){
+        LoginResponse loginResponse = this.userService.reIssueToken(userIdx);
+        return ResponseEntity.ok(ResponseDto.create(UserConstants.EBoardResponseMessage.REISSUE_SUCCESS.getMessage(), loginResponse));
+    }
 }
--- src/main/java/com/example/hackathon/domain/user/service/UserService.java
+++ src/main/java/com/example/hackathon/domain/user/service/UserService.java
@@ -4,10 +4,13 @@ import com.example.hackathon.domain.user.dto.UserDto;
 import com.example.hackathon.domain.user.dto.UserDto.LoginRequest;
 import com.example.hackathon.domain.user.dto.UserDto.LoginResponse;
 import com.example.hackathon.domain.user.entity.User;
+import com.example.hackathon.global.dto.TokenInfoResponse;
 
 public interface UserService {
 
     void signup(UserDto.SignupRequest signupRequest);
     LoginResponse login(LoginRequest loginRequest);
     User validateUserId(String userId);
+    LoginResponse reIssueToken(Long userIdx);
+    //재발급
 }
